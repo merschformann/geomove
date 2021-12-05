fc934892d6e6fa9b1131546615c073890d3771bb
Marius Merschformann <marius.merschformann@gmail.com>
2021-12-05 05:14:56 +0100

    Init project

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # geomove
-Small library for moving geolocations towards a bearing.
+
+Moves points on earth towards a given bearing by a given distance.
+
+## Installation
+
+```bash
+pip install geomove
+```
+
+## Usage
+
+Move a point west by 10 km:
+
+```python
+from geomove import move, Bearing
+
+# Define point
+point = (13.4, 52.5)
+
+# Move 
+moved_point = move(point, Bearing.WEST, 10)
+```
+
+Move a point towards 357° by 5 km:
+
+```python
+from geomove import move
+
+# Define point
+point = (13.4, 52.5)
+
+# Move
+moved_point = move(point, 357, 5)
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("requirements.txt") as fp:
+    install_requires = fp.read()
+
+with open("version.txt") as fp:
+    version = fp.readline().strip()
+
+with open("README.md") as fp:
+    readme = fp.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setuptools.setup(
+    name="geomove",
+    description="Moves points on earth towards a given bearing by a given distance.",
+    long_description=readme,
+    version=version,
+    license=license,
+    author="Marius Merschformann",
+    author_email="marius.merschformann@gmail.com",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+)
