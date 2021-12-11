76014831cc06ef3c92a634be84d5793e3e2a7130
Marius Merschformann <marius.merschformann@gmail.com>
2021-12-11 02:16:31 +0100

    Fixing tests, allowing some more tolerance

--- tests/test_move.py
+++ tests/test_move.py
@@ -9,19 +9,20 @@ def test_move_point():
     # Define cases (origin, bearing, distance, expected)
     cases = [
         ((51.9624, 7.6256), 0, 1, (51.97139320367762, 7.625600000000001)),
-        ((51.9624, 7.6256), 90, 1, (51.95836966175978, 7.638646822807718)),
+        ((51.9624, 7.6256), 90, 1, (51.96239909784941, 7.640195127867031)),
         (
             (0, -179.999999),
             Bearing.WEST,
             10,
-            (0.0885274726594929, 179.98416881686882),
+            (0.0, 179.9100689632238),
         ),
     ]
     # Run test cases
     for case in cases:
         moved = move(case[0], case[1], case[2])
-        assert moved == pytest.approx(case[3], abs=1e-12)
-        assert haversine(case[0], moved) == pytest.approx(case[2], abs=1e-12)
+        assert moved == pytest.approx(case[3], abs=1e-10)
+        dist = haversine(case[0], moved)
+        assert dist == pytest.approx(case[2], abs=1e-10)
 
 
 if __name__ == "__main__":
